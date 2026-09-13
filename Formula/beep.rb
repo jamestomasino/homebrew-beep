8e748bda2a7afbaf4ba47330fc306383fdf43e7b
James Tomasino <james@tomasino.org>
2026-09-13 19:09:14 +0000

    v0.1.8

--- Formula/beep.rb
+++ Formula/beep.rb
@@ -1,19 +1,19 @@
 class Beep < Formula
   desc "Activity sonifier CLI"
   homepage "https://github.com/jamestomasino/beep"
-  url "https://github.com/jamestomasino/beep/releases/download/v0.1.7/beep-v0.1.7-linux-x86_64.tar.gz"
-  version "0.1.7"
-  sha256 "3933ccbe02cdd164d9c040621fffa7c8b862180d58c0748e46256c86745a1b1f"
+  url "https://github.com/jamestomasino/beep/releases/download/v0.1.8/beep-v0.1.8-linux-x86_64.tar.gz"
+  version "0.1.8"
+  sha256 "b9845bc5816b199892e453c53b26bd00cc0561830458c56b63d52134f4e00adc"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/jamestomasino/beep/releases/download/v#{version}/beep-v#{version}-darwin-arm64.tar.gz"
-      sha256 "abd272bb2898d9f3bd87cef6fd605d70d7bad00eabdeacd793cb1136f4ee8b74"
+      sha256 "90bf070bfe80b3b79d9ec19c824a21b3560bd0cd2a311ee2bd8e3fad160c3e0e"
     end
     on_intel do
       url "https://github.com/jamestomasino/beep/releases/download/v#{version}/beep-v#{version}-darwin-x86_64.tar.gz"
-      sha256 "3c8752803e9313f136dbf2a4df8d969779b2af4596ca594282e308f4eb1f8283"
+      sha256 "1f2f74dc340616837687b7d675a5f27513a0953d425f655c78f5a1ba330e4450"
     end
   end
 
